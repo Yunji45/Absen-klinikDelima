7ac25d41489a41a7f2446f58bbc4f23d487f1a52
Yunji45 <ihyanatikwibowo@gmail.com>
2023-09-24 01:49:24 +0700

    coba ip baru ah

--- config/absensi.php
+++ config/absensi.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ip_internet'    => env('IP_ADDRESS', '192.168.239.1'),
+    'ip_internet'    => env('IP_ADDRESS', '114.5.219.119'),
     'jam_masuk'     => env('JAM_MASUK', '00:00'),
     'jam_keluar'    => env('JAM_KELUAR', '24:00'),
 
