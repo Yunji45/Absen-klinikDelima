0b8308366a45525a11c07766fb0b0c1ea42bce29
Yunji45 <ihyanatikwibowo@gmail.com>
2023-09-26 02:45:42 +0700

    aduh lupa lagi dah

--- resources/views/frontend/users/userpresensi.blade.php
+++ resources/views/frontend/users/userpresensi.blade.php
@@ -50,14 +50,19 @@
                     @endif @if($present->jam_keluar)
                     <td>{{ date('H:i:s', strtotime($present->jam_keluar)) }}</td>
                     <td>
-                        @if (strtotime($present->jam_keluar) <= strtotime($present->jam_masuk))
+                        <!-- @if (strtotime($present->jam_keluar) <= strtotime($present->jam_masuk))
                         {{ 21 - (\Carbon\Carbon::parse($present->jam_masuk)->diffInHours(\Carbon\Carbon::parse($present->jam_keluar))) }}
                         @else @if (strtotime($present->jam_keluar) >=
                         strtotime(config('absensi.jam_pulang') . ' +2 hours'))
                         {{ (\Carbon\Carbon::parse($present->jam_masuk)->diffInHours(\Carbon\Carbon::parse($present->jam_keluar))) - 3 }}
                         @else
                         {{ (\Carbon\Carbon::parse($present->jam_masuk)->diffInHours(\Carbon\Carbon::parse($present->jam_keluar))) - 1 }}
-                        @endif @endif
+                        @endif @endif -->
+                        @php $jamMasuk = \Carbon\Carbon::parse($present->jam_masuk);
+                            $jamKeluar = \Carbon\Carbon::parse($present->jam_keluar);
+                            $totalJam = $jamMasuk->diffInHours($jamKeluar);
+                        @endphp
+                        {{$totalJam}}
                     </td>
                     @else
                     <td>-</td>
