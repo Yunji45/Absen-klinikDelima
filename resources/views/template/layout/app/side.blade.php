913fadc5717bcc5ede45ad34ecbeed1981589120
Yunji45 <ihyanatikwibowo@gmail.com>
2023-10-14 04:53:06 +0700

    Nyicil FE Sidebar

--- resources/views/template/layout/app/side.blade.php
+++ resources/views/template/layout/app/side.blade.php
@@ -10,12 +10,55 @@
             <li class="menu-header">Dashboard</li>
             <li>
                 <a class="nav-link" href="{{route('kpi.index')}}">
-                    <i class="far fa-square"></i>
+                    <i class="fa fa-palette"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
             <li class="menu-header">SDM</li>
             @if (auth()->user()->role == 'admin')
+            <li>
+                <a class="nav-link" href="">
+                    <i class="fa fa-user"></i>
+                    <span>Kehadiran</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="">
+                    <i class="far fa-square"></i>
+                    <span>Manage User</span>
+                </a>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown">
+                    <i class="fas fa-columns"></i>
+                    <span>Pengaturan Jaga</span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="nav-link" href="">Jadwal Jaga</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="">Permohonan Izin</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="">Perubahan Jaga</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown">
+                    <i class="fas fa-columns"></i>
+                    <span>Informasi Pegawai</span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a class="nav-link" href="">Detail Pegawai</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="">Dokumen</a>
+                    </li>
+                </ul>
+            </li>
             <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown">
                     <i class="fas fa-columns"></i>
@@ -28,19 +71,29 @@
                     <li>
                         <a class="nav-link" href="{{route('kpi.index')}}">KPI</a>
                     </li>
+                    <li>
+                        <a class="nav-link" href="">Setup UMR</a>
+                    </li>
                 </ul>
             </li>
-            @endif
             <li>
                 <a class="nav-link" href="">
                     <i class="far fa-square"></i>
                     <span>Blank Page</span>
                 </a>
             </li>
+            @endif
+            <li class="menu-header">HELP IT</li>
             <li>
                 <a class="nav-link" href="">
                     <i class="fas fa-pencil-ruler"></i>
-                    <span>Credits</span>
+                    <span>Documentasi API</span>
+                </a>
+            </li>
+            <li>
+                <a class="nav-link" href="">
+                    <i class="fas fa-pencil-ruler"></i>
+                    <span>Help IT</span>
                 </a>
             </li>
         </ul>
