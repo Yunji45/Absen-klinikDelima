e31ff4e083114b6daac5411f7c8e04c9b9372d95
Yunji45 <ihyanatikwibowo@gmail.com>
2023-10-20 14:39:28 +0700

    bla bla bla config

--- config/absensi.php
+++ config/absensi.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ip_internet'    => env('IP_ADDRESS', '36.74.40.150'),
+    'ip_internet'    => env('IP_ADDRESS', '36.74.45.164'),
     
     'jam_masuk'     => env('JAM_MASUK', '00:00'),
     'jam_keluar'    => env('JAM_KELUAR', '24:00'),
