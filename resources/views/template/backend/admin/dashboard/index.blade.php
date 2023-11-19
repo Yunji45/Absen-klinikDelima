3c698780cb695c755a445c2e31a9caa1a817b3a7
Yunji45 <ihyanatikwibowo@gmail.com>
2023-11-19 19:55:26 +0700

    fshj ganti fashhfds

--- resources/views/template/backend/admin/dashboard/index.blade.php
+++ resources/views/template/backend/admin/dashboard/index.blade.php
@@ -168,45 +168,6 @@
               <div class="col-lg-12 col-md-12 col-12 col-sm-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>Statistics Pendidikan</h4>
-                    <div class="card-header-action">
-                      <a href="#" class="btn active">Week</a>
-                      <a href="#" class="btn">Month</a>
-                      <a href="#" class="btn">Year</a>
-                    </div>
-                  </div>
-                  <div class="card-body">
-                    <canvas id="myChart6" height="180"></canvas>
-                    <div class="statistic-details mt-1">
-                      <div class="statistic-details-item">
-                        <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 7%</div>
-                        <div class="detail-value">$243</div>
-                        <div class="detail-name">Today</div>
-                      </div>
-                      <div class="statistic-details-item">
-                        <div class="text-small text-muted"><span class="text-danger"><i class="fas fa-caret-down"></i></span> 23%</div>
-                        <div class="detail-value">$2,902</div>
-                        <div class="detail-name">This Week</div>
-                      </div>
-                      <div class="statistic-details-item">
-                        <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span>9%</div>
-                        <div class="detail-value">$12,821</div>
-                        <div class="detail-name">This Month</div>
-                      </div>
-                      <div class="statistic-details-item">
-                        <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 19%</div>
-                        <div class="detail-value">$92,142</div>
-                        <div class="detail-name">This Year</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          </div>
-          <div class="row">
-              <div class="col-lg-12 col-md-12 col-12 col-sm-12">
-                <div class="card">
-                  <div class="card-header">
                     <h4>Statistics Kepegawaian</h4>
                     <div class="card-header-action">
                       <a href="#" class="btn active">Week</a>
@@ -243,59 +204,15 @@
               </div>
           </div>
           <div class="row">
-            <div class="col-lg-6 col-md-12 col-12 col-sm-12">
-              <div class="card">
-                <div class="card-body pt-2 pb-2">
-                  <div id="myWeather">Please wait</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-12 col-12 col-sm-12">
-              <div class="card">
-                <div class="card-header">
-                  <h4>Authors</h4>
-                </div>
-                <div class="card-body">
-                  <div class="row pb-2">
-                    <div class="col-6 col-sm-3 col-lg-3 mb-4 mb-md-0">
-                      <div class="avatar-item mb-0">
-                        <img alt="image" src="assets/img/avatar/avatar-5.png" class="img-fluid" data-toggle="tooltip" title="Alfa Zulkarnain">
-                        <div class="avatar-badge" title="Editor" data-toggle="tooltip"><i class="fas fa-wrench"></i></div>
-                      </div>
-                    </div>
-                    <div class="col-6 col-sm-3 col-lg-3 mb-4 mb-md-0">
-                      <div class="avatar-item mb-0">
-                        <img alt="image" src="assets/img/avatar/avatar-4.png" class="img-fluid" data-toggle="tooltip" title="Egi Ferdian">
-                        <div class="avatar-badge" title="Admin" data-toggle="tooltip"><i class="fas fa-cog"></i></div>
-                      </div>
-                    </div>
-                    <div class="col-6 col-sm-3 col-lg-3 mb-4 mb-md-0">
-                      <div class="avatar-item mb-0">
-                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="img-fluid" data-toggle="tooltip" title="Jaka Ramadhan">
-                        <div class="avatar-badge" title="Author" data-toggle="tooltip"><i class="fas fa-pencil-alt"></i></div>
-                      </div>
-                    </div>
-                    <div class="col-6 col-sm-3 col-lg-3 mb-4 mb-md-0">
-                      <div class="avatar-item mb-0">
-                        <img alt="image" src="assets/img/avatar/avatar-2.png" class="img-fluid" data-toggle="tooltip" title="Ryan">
-                        <div class="avatar-badge" title="Admin" data-toggle="tooltip"><i class="fas fa-cog"></i></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
             <div class="col-lg-6 col-md-6 col-12">
               <div class="card">
                 <div class="card-header">
-                  <h4>Referral URL</h4>
+                  <h4>Pendidikan</h4>
                 </div>
                 <div class="card-body">
                   <div class="mb-4">
                     <div class="text-small float-right font-weight-bold text-muted">2,100</div>
-                    <div class="font-weight-bold mb-1">Google</div>
+                    <div class="font-weight-bold mb-1">S2</div>
                     <div class="progress" data-height="3">
                       <div class="progress-bar" role="progressbar" data-width="80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>                          
@@ -303,7 +220,7 @@
 
                   <div class="mb-4">
                     <div class="text-small float-right font-weight-bold text-muted">1,880</div>
-                    <div class="font-weight-bold mb-1">Facebook</div>
+                    <div class="font-weight-bold mb-1">S1 Kesehatan</div>
                     <div class="progress" data-height="3">
                       <div class="progress-bar" role="progressbar" data-width="67%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
@@ -311,7 +228,7 @@
 
                   <div class="mb-4">
                     <div class="text-small float-right font-weight-bold text-muted">1,521</div>
-                    <div class="font-weight-bold mb-1">Bing</div>
+                    <div class="font-weight-bold mb-1">S1 Non Kesehatan</div>
                     <div class="progress" data-height="3">
                       <div class="progress-bar" role="progressbar" data-width="58%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
@@ -319,7 +236,7 @@
 
                   <div class="mb-4">
                     <div class="text-small float-right font-weight-bold text-muted">884</div>
-                    <div class="font-weight-bold mb-1">Yahoo</div>
+                    <div class="font-weight-bold mb-1">D3 Kesehatan</div>
                     <div class="progress" data-height="3">
                       <div class="progress-bar" role="progressbar" data-width="36%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
@@ -327,7 +244,7 @@
 
                   <div class="mb-4">
                     <div class="text-small float-right font-weight-bold text-muted">473</div>
-                    <div class="font-weight-bold mb-1">Kodinger</div>
+                    <div class="font-weight-bold mb-1">D3 Non Kesehatan</div>
                     <div class="progress" data-height="3">
                       <div class="progress-bar" role="progressbar" data-width="28%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
@@ -335,7 +252,21 @@
 
                   <div class="mb-4">
                     <div class="text-small float-right font-weight-bold text-muted">418</div>
-                    <div class="font-weight-bold mb-1">Multinity</div>
+                    <div class="font-weight-bold mb-1">SLTA Kesehatan</div>
+                    <div class="progress" data-height="3">
+                      <div class="progress-bar" role="progressbar" data-width="20%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="mb-4">
+                    <div class="text-small float-right font-weight-bold text-muted">418</div>
+                    <div class="font-weight-bold mb-1">SLTA Non Kesehatan</div>
+                    <div class="progress" data-height="3">
+                      <div class="progress-bar" role="progressbar" data-width="20%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="mb-4">
+                    <div class="text-small float-right font-weight-bold text-muted">418</div>
+                    <div class="font-weight-bold mb-1">Dibawah SLTA</div>
                     <div class="progress" data-height="3">
                       <div class="progress-bar" role="progressbar" data-width="20%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
