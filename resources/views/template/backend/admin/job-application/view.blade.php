031b803a1bdfec6d22c8f7dbed2d0b61d19cd1e1
Yunji45 <ihyanatikwibowo@gmail.com>
2024-01-25 16:46:53 +0700

    perubahan title di show kandidat

--- resources/views/template/backend/admin/job-application/view.blade.php
+++ resources/views/template/backend/admin/job-application/view.blade.php
@@ -1,7 +1,7 @@
 @extends('template.layout.app.main') @section('tabel')
 <section class="section">
     <div class="section-header">
-        <h1>Detail Pegawai</h1>
+        <h1>Profil Kandidat {{$detail->nama_lengkap}}</h1>
         <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active">
                 <a href="#">Dashboard</a>
