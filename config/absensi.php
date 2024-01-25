f3470885148ebaf6b3f90dc52c3018f73a3e782a
Yunji45 <ihyanatikwibowo@gmail.com>
2024-01-25 15:35:38 +0700

    perbaiki config name ip

--- config/absensi.php
+++ config/absensi.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ip_internet'    => env('IP_ADDRESS', '36.81.218.187'),
+    'ip_internet'    => env('IP_ADDRESS', '36.74.45.18'),
     
     'jam_masuk'     => env('JAM_MASUK', '00:00'),
     'jam_keluar'    => env('JAM_KELUAR', '24:00'),
