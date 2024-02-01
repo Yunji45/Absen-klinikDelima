4a49170404218be83896d91f4762e96142205d3f
Yunji45 <ihyanatikwibowo@gmail.com>
2024-02-01 09:27:00 +0700

    ketinggal config

--- config/absensi.php
+++ config/absensi.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ip_internet'    => env('IP_ADDRESS', '36.74.45.89'),
+    'ip_internet'    => env('IP_ADDRESS', '36.72.238.126'),
     
     'jam_masuk'     => env('JAM_MASUK', '00:00'),
     'jam_keluar'    => env('JAM_KELUAR', '24:00'),
