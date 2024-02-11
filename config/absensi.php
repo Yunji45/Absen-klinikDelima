f6a4f4d0432e49d1d0c85304ba3ab1f6b5319624
Yunji45 <ihyanatikwibowo@gmail.com>
2024-02-11 07:01:38 +0700

    KPI:deploy delete all

--- config/absensi.php
+++ config/absensi.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ip_internet'    => env('IP_ADDRESS', '36.72.238.126'),
+    'ip_internet'    => env('IP_ADDRESS', '180.252.29.60'),
     
     'jam_masuk'     => env('JAM_MASUK', '00:00'),
     'jam_keluar'    => env('JAM_KELUAR', '24:00'),
