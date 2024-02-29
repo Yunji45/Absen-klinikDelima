1427903ab7fd834a30c3cc9a4a3a7b11b678312e
Yunji45 <ihyanatikwibowo@gmail.com>
2024-03-01 06:06:01 +0700

    gaji:typo di get data

--- config/absensi.php
+++ config/absensi.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ip_internet'    => env('IP_ADDRESS', '36.74.42.62'),
+    'ip_internet'    => env('IP_ADDRESS', '180.252.26.150'),
     
     'jam_masuk'     => env('JAM_MASUK', '00:00'),
     'jam_keluar'    => env('JAM_KELUAR', '24:00'),
