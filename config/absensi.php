dd2c14339f5046b781f66cc774629273d02db7aa
Yunji45 <ihyanatikwibowo@gmail.com>
2024-05-09 02:25:56 +0700

    dash_layanan:coba deploy

--- config/absensi.php
+++ config/absensi.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ip_internet'    => env('IP_ADDRESS', '36.75.82.44'),
+    'ip_internet'    => env('IP_ADDRESS', '36.81.228.79'),
     
     'jam_masuk'     => env('JAM_MASUK', '00:00'),
     'jam_keluar'    => env('JAM_KELUAR', '24:00'),
