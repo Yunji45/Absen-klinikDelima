5339c26baa998133a37eb67ba0dac631e75715fb
Yunji45 <ihyanatikwibowo@gmail.com>
2024-06-22 12:12:08 +0700

    auth:ubah color login

--- config/absensi.php
+++ config/absensi.php
@@ -1,11 +1,17 @@
-<?php return array (
-  'ip_internet' => '36.74.40.218',
-  'jam_masuk' => '00:00',
-  'jam_keluar' => '24:00',
-  'jam_masuk_PS' => '07:00',
-  'jam_keluar_PS' => '16:00',
-  'jam_masuk_SM' => '16:00',
-  'jam_keluar_SM' => '07:00',
-  'jam_masuk_PM' => '07:00',
-  'jam_keluar_PM' => '22:00',
-);
+<?php
+
+return [
+    'ip_internet'    => env('IP_ADDRESS', '36.74.41.227'),
+    
+    'jam_masuk'     => env('JAM_MASUK', '00:00'),
+    'jam_keluar'    => env('JAM_KELUAR', '24:00'),
+
+    'jam_masuk_PS'  => env('JAM_MASUK_PS', '07:00'),
+    'jam_keluar_PS'  => env('JAM_KELUAR_PS', '16:00'),
+
+    'jam_masuk_SM'  => env('JAM_MASUK_SM', '16:00'),
+    'jam_keluar_SM'  => env('JAM_KELUAR_SM', '07:00'),
+    
+    'jam_masuk_PM'  => env('JAM_MASUK_PM', '07:00'),
+    'jam_keluar_PM'  => env('JAM_KELUAR_PM', '22:00'),
+];
